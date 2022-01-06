368f24813edcc6902c6a8e89d8b392ba193cfae4
Johnsensei <john@johnsensei.com>
2022-01-06 15:50:13 -0600

    FlatList

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [goalList, setGoalList] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    // console.log(enteredGoal);
+    setGoalList(currentGoals => [...currentGoals,
+      {key: Math.random().toString(), value: enteredGoal}]);
+  }
+
   return (
     <View style={styles.mainContainer}>
       <View style={styles.goalInputContainer}>
         <TextInput
           placeholder='Input goal.'
           style={styles.goalInputBox}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
         <Button
           title="ADD"
+          onPress={addGoalHandler}
         />
       </View>
-      <View>
-
-      </View>
+      <FlatList
+        data={goalList}
+        renderItem={itemData => 
+          <View key={itemData.key} style={styles.goalItem}>
+            <Text >
+              {itemData.item.value}
+            </Text>
+          </View>
+        }
+      />
     </View>
   );
 }
@@ -23,6 +47,7 @@ export default function App() {
 const styles = StyleSheet.create({
   mainContainer: {
     padding: 50,
+    marginTop: 50
   },
   goalInputContainer: {
     flexDirection: 'row',
@@ -34,5 +59,12 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  goalItem: {
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 10
   }
 });
