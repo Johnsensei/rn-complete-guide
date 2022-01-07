be67ce6c6f1b2132ad9e9d14be5972aa8c8dec02
Johnsensei <john@johnsensei.com>
2022-01-07 13:49:08 -0600

    Passing Data Between Components

--- App.js
+++ App.js
@@ -1,36 +1,25 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
 
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [goalList, setGoalList] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  }
-
-  const addGoalHandler = () => {
+  const addGoalHandler = goalTitle => {
     // console.log(enteredGoal);
     setGoalList(currentGoals => [...currentGoals,
-      {key: Math.random().toString(), value: enteredGoal}]);
+      {key: Math.random().toString(), value: goalTitle}]);
   }
 
   return (
     <View style={styles.mainContainer}>
-      <View style={styles.goalInputContainer}>
-        <TextInput
-          placeholder='Input goal.'
-          style={styles.goalInputBox}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button
-          title="ADD"
-          onPress={addGoalHandler}
-        />
-      </View>
+
+      <GoalInput
+        onAddGoal={addGoalHandler}
+      />
+
       <FlatList
       // keyExtractor={(item, index) => item.id}
         data={goalList}
@@ -50,22 +39,4 @@ const styles = StyleSheet.create({
     padding: 50,
     marginTop: 50
   },
-  goalInputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  goalInputBox: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  },
-  goalItem: {
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10,
-    marginTop: 10
-  }
 });
